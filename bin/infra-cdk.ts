57be1ad270a01215736b47b45cb8329c376c1b6b
Enrique M. Talavera <kiquetal@pop-os.localdomain>
2021-07-02 23:17:33 -0400

    Initial commit

---
+++ bin/infra-cdk.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { InfraCdkStack } from '../lib/infra-cdk-stack';
+
+const app = new cdk.App();
+new InfraCdkStack(app, 'InfraCdkStack');
